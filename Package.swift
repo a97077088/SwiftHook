457ba727eb8922ec8f644177203780189b525509
a97077088 <97077088@qq.com>
2021-05-17 16:25:44 +0800

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "SwiftHook",
+    platforms: [
+       .iOS(.v12),
+       .macOS(.v10_13),
+       .tvOS(.v11),
+       .watchOS(.v5)
+     ],
+    products: [
+        .library(name: "SwiftHook", targets: ["SwiftHook"]),
+    ],
+    targets: [
+        .target(name: "SuperBuilder"),
+        .target(name: "SwiftHook", dependencies: ["SuperBuilder"]),
+        .testTarget(name: "SwiftHookTests", dependencies: ["SwiftHook"]),
+    ]
+)
